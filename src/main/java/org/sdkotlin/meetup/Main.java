f226fc5e4c036b393f0941fb85dc147f4099955b
Ian Brandt <ian@ianbrandt.com>
2018-01-17 21:46:11 -0800

    Hello, World!

---
+++ src/main/java/org/sdkotlin/meetup/Main.java
@@ -0,0 +1,7 @@
+package org.sdkotlin.meetup;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello, SD Kotlin!");
+	}
+}
---
+++ src/main/kotlin/org/sdkotlin/meetup/Main.kt
@@ -0,0 +1,5 @@
+package org.sdkotlin.meetup
+
+fun main(args: Array<String>) {
+	println("Hello, SD Kotlin!")
+}
