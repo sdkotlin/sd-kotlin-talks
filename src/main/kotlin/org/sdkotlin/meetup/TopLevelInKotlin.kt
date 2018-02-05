0193a29ca27f86e232560a6bbdeb67aba32b9c33
Ian Brandt <ian@ianbrandt.com>
2018-02-05 08:12:49 -0800

    Added top-level variables and functions in Kotlin.

---
+++ src/main/kotlin/org/sdkotlin/meetup/TopLevelInKotlin.kt
@@ -0,0 +1,11 @@
+package org.sdkotlin.meetup
+
+// You may define top level variables and functions in Kotlin
+
+var sanDiego = true
+
+fun printWeather() {
+	if (sanDiego) {
+		println("It's sunny!")
+	}
+}
