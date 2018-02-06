4da138810c4147bc0b927d9fa1bc78da2866e0f1
Ian Brandt <ian@ianbrandt.com>
2018-02-05 20:20:24 -0800

    Added basic types.

---
+++ src/main/java/org/sdkotlin/meetup/TypesInJava.java
@@ -0,0 +1,20 @@
+package org.sdkotlin.meetup;
+
+public class TypesInJava {
+	boolean bool; // = false
+	Boolean Bool; // = null
+	byte b; // = 0
+	Byte B; // = null
+	char c; // = 'u0000', i.e. NULL
+	Character C; // = null
+	short s; // = 0
+	Short S; // = null
+	int i; // = 0
+	Integer I; // = null
+	long l; // = 0L
+	Long L; // = null
+	float f; // = 0.0f
+	Float F; // = null
+	double d; // = 0.0d
+	Double D; // = null
+}
---
+++ src/main/kotlin/org/sdkotlin/meetup/TypesInKotlin.kt
@@ -0,0 +1,10 @@
+package org.sdkotlin.meetup
+
+var bool = false // Boolean
+var b = 0.toByte() // Byte, no literal
+var c = 'c' // Char
+var s = 0.toShort() // Short, no literal
+var i = 0 // Int
+var l = 0L // Long, lowercase 'l' not supported
+var f = 0.0f // Float, 'F' also supported
+var d = 0.0 // Double, 'd/D' not supported
