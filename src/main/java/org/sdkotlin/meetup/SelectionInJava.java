089e58568c9a8f51065e3c44bda1d4f31ff5dd9d
Ian Brandt <ian@ianbrandt.com>
2018-02-07 16:49:47 -0800

    Added selection.

---
+++ src/main/java/org/sdkotlin/meetup/SelectionInJava.java
@@ -0,0 +1,27 @@
+package org.sdkotlin.meetup;
+
+public class SelectionInJava {
+	public static void main(String[] args) {
+
+		if (true) {
+			System.out.println(true);
+		} else if (false) {
+			System.out.println(false);
+		} else {
+			System.out.println("Huh?");
+		}
+
+		int x = (true) ? 1 : 0;
+
+		switch (x) {
+			case 0:
+				System.out.println("x == 0");
+			case 1:
+				System.out.println("x == 1");
+			case 2:
+				System.out.println("x == 1 (Oops, forgot the break!)");
+			default:
+				System.out.println("Double oops!");
+		}
+	}
+}
---
+++ src/main/kotlin/org/sdkotlin/meetup/SelectionInKotlin.kt
@@ -0,0 +1,26 @@
+package org.sdkotlin.meetup
+
+fun main(args: Array<String>) {
+
+	// If blocks in Kotlin are as in Java
+	if (true) {
+		println(true)
+	} else if (false) {
+		println(false)
+	} else {
+		println("Huh?")
+	}
+
+	// Except they're expressions!
+	// As such no ternary operator
+	val x = if (true) 1 else 0
+
+	when (x) {
+		0, 1 -> println("x is 0 or 1")
+		in 2..10 -> println("x between 2 and 10 (inclusive)")
+		is Number -> println("x is a number")
+		else -> { // Note the block
+			println("x is neither 1 nor 2")
+		}
+	}
+}
