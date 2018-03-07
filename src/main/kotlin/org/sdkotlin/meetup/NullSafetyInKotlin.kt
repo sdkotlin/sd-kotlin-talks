ea1d7403593af3e30d81b4689889704346fe4a42
Ian Brandt <ian@ianbrandt.com>
2018-03-07 10:17:04 -0800

    Added null safety.

---
+++ src/main/kotlin/org/sdkotlin/meetup/NullSafetyInKotlin.kt
@@ -0,0 +1,46 @@
+package org.sdkotlin.meetup
+
+fun main(args: Array<String>) {
+
+	var name = "Ezra"
+	// name = null // Does not compile
+
+	var nullableName: String? = null
+
+	println("Non-nullable name length: ${name.length}")
+
+	// println(nullableName.length) // Does not compile
+	println("Safe nullable name length: ${nullableName?.length}")
+
+	if (nullableName != null) {
+		println("Smart casted nullable name length: ${nullableName.length}")
+	}
+
+	val possibleNoNames = listOf("Ezra", null, "Luke")
+
+	for (possibleName in possibleNoNames) {
+		possibleName?.let { println("Let's only print non-null names: ${it}") }
+	}
+
+	for (possibleName in possibleNoNames) {
+		val defaultedName = possibleName ?: "Elvis"
+		println("""Or we could use the "Elvis" operator to default null names: $defaultedName""")
+	}
+
+	try {
+		val requiredName =
+			nullableName ?: throw IllegalArgumentException("Or we could throw a better exception for missing arguments")
+	} catch (e: IllegalArgumentException) {
+		println(e.message)
+	}
+
+	for (definiteName in possibleNoNames.filterNotNull()) {
+		println("Or we could use filtering and smart casting: ${definiteName.length}")
+	}
+
+	try {
+		nullableName!!.length
+	} catch (e: NullPointerException) {
+		println("Or we could really just love NullPointerExceptions!!")
+	}
+}
