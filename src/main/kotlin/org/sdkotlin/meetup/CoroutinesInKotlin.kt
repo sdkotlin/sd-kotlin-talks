f38f53ec9a2095b48bb8a37df77eb963381ed363
Ian Brandt <ian@ianbrandt.com>
2018-04-04 16:40:53 -0700

    Expanded on coroutines.

--- src/main/kotlin/org/sdkotlin/meetup/CoroutinesInKotlin.kt
+++ src/main/kotlin/org/sdkotlin/meetup/CoroutinesInKotlin.kt
@@ -1,18 +1,68 @@
 package org.sdkotlin.meetup
 
+import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.runBlocking
+import java.lang.System.gc
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.concurrent.thread
 
-fun main(args: Array<String>) = runBlocking {
+fun main(args: Array<String>) {
 
-	// Launch a lot of coroutines!
-	val jobs = List(100_000) {
-		launch {
-			delay(1000L)
-			println(".")
+	//lotsOfThreads() // OutOfMemoryError!
+
+	lotsOfCoroutines()
+
+	asyncStyle()
+}
+
+private fun lotsOfThreads() {
+
+	(1..100_000).forEach {
+
+		try {
+			thread {
+				Thread.sleep(1000)
+			}
+
+		} catch (e: OutOfMemoryError) {
+			println("I can't do it, Captain!  ${it} is just too many threads!")
+			return
 		}
 	}
+}
 
-	jobs.forEach { it.join() } // wait for all jobs to complete
+private fun lotsOfCoroutines() {
+
+	runBlocking {
+
+		// Launch a lot of coroutines!
+		val jobs = List(1_000_000) {
+
+			launch {
+				delay(1000)
+			}
+		}
+
+		jobs.forEach { it.join() } // wait for all jobs to complete
+
+		println("No trouble creating ${jobs.size} coroutines!")
+	}
+}
+
+private fun asyncStyle() {
+
+	runBlocking {
+
+		val deferredInts = List(1_000_000) { i ->
+			async {
+				i + 1
+			}
+		}
+
+		val sum = deferredInts.sumBy { deferred -> deferred.await() }
+
+		println("The sum of all numbers between 1 and ${deferredInts.size} is $sum.")
+	}
 }
