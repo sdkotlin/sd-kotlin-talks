e83022d1e51d4a4e4f1a484731c4e2217962914c
Ian Brandt <ian@ianbrandt.com>
2018-06-14 16:10:30 -0700

    Upgraded coroutines.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,13 +14,14 @@ version = "1.0-SNAPSHOT"
 
 repositories {
 	mavenCentral()
+	jcenter()
 }
 
 dependencies {
 	val junitVersion = "5.2.0"
 
 	implementation(kotlin("stdlib-jdk8"))
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:0.23.1")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:0.23.2")
 	testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 	testImplementation("org.assertj:assertj-core:3.10.0")
