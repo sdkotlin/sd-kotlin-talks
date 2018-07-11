193ea28affbc0751087bf19ceffe32518281ee44
Ian Brandt <ian@ianbrandt.com>
2018-07-11 11:32:10 -0700

    Upgraded Kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("jvm") version "1.2.50"
+	kotlin("jvm") version "1.2.51"
 	id("com.github.ben-manes.versions") version "0.20.0"
 }
 
