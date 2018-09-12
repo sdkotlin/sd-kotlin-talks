22a172bdcba89d0d0f415b913817df2bfd881235
Ian Brandt <ian@ianbrandt.com>
2018-09-12 09:47:35 -0700

    Upgraded AssertJ.

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 
 	implementation(kotlin("stdlib-jdk8"))
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:0.24.0")
-	testImplementation("org.assertj:assertj-core:3.10.0")
+	testImplementation("org.assertj:assertj-core:3.11.1")
 	testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
