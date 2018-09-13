97c9e16654af1b47f8942620df4342f3d4b06a25
Ian Brandt <ian@ianbrandt.com>
2018-09-13 11:58:42 -0700

    Updated coroutines example.

--- src/main/kotlin/org/sdkotlin/meetup/CoroutinesInKotlin.kt
+++ src/main/kotlin/org/sdkotlin/meetup/CoroutinesInKotlin.kt
@@ -1,10 +1,11 @@
 package org.sdkotlin.meetup
 
-import kotlinx.coroutines.experimental.async
-import kotlinx.coroutines.experimental.delay
-import kotlinx.coroutines.experimental.launch
-import kotlinx.coroutines.experimental.runBlocking
-import kotlin.concurrent.thread
+import kotlinx.coroutines.experimental.*
+import kotlin.concurrent.*
+import kotlin.system.*
+
+const val ONE_THOUSAND = 1_000
+const val ONE_MILLION = 1_000_000
 
 fun main(args: Array<String>) {
 
@@ -12,20 +13,19 @@ fun main(args: Array<String>) {
 
 	lotsOfCoroutines()
 
-	asyncStyle()
+	asyncSum()
 }
 
 private fun lotsOfThreads() {
 
-	(1..100_000).forEach {
+	(1..ONE_MILLION).forEach {
 
 		try {
 			thread {
-				Thread.sleep(1000)
+				Thread.sleep(ONE_THOUSAND.toLong())
 			}
-
 		} catch (e: OutOfMemoryError) {
-			println("I can't do it, Captain!  ${it} is just too many threads!")
+			println("I can't do it, Captain!  $it is just too many threads!")
 			return
 		}
 	}
@@ -33,34 +33,41 @@ private fun lotsOfThreads() {
 
 private fun lotsOfCoroutines() {
 
-	runBlocking {
-
+	val elapsedTime = measureTimeMillis {
 		// Launch a lot of coroutines!
-		val jobs = List(1_000_000) {
+		val jobs = List(ONE_MILLION) {
 
-			launch {
-				delay(1000)
+			GlobalScope.launch {
+				delay(ONE_THOUSAND)
 			}
 		}
 
-		jobs.forEach { it.join() } // wait for all jobs to complete
+		runBlocking {
 
-		println("No trouble creating ${jobs.size} coroutines!")
+			jobs.forEach { it.join() } // wait for all jobs to complete
+		}
+
 	}
+
+	println(
+		"Created $ONE_MILLION coroutines that sleep for $ONE_THOUSAND milliseconds in $elapsedTime total milliseconds!")
 }
 
-private fun asyncStyle() {
+private fun asyncSum() {
 
-	runBlocking {
+	val elapsedTime = measureTimeMillis {
 
-		val deferredInts = List(1_000_000) { i ->
-			async {
+		val deferredInts = List(ONE_MILLION) { i ->
+			GlobalScope.async {
 				i + 1
 			}
 		}
 
-		val sum = deferredInts.sumBy { deferred -> deferred.await() }
-
-		println("The sum of all numbers between 1 and ${deferredInts.size} is $sum.")
+		runBlocking {
+			deferredInts.awaitAll()
+		}
 	}
+
+	println("The async initialization of a list with sequential numbers between 1 and $ONE_MILLION " +
+		"took $elapsedTime milliseconds.")
 }
