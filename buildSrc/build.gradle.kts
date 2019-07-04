4be4f8f5f73b7477ef8d8cfbc0109fe11b393f56
Ian Brandt <ian@ianbrandt.com>
2019-07-03 19:13:36 -0700

    Upgraded Kotlin.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-	implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.40")
+	implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.41")
 }
 
 kotlinDslPluginOptions {
