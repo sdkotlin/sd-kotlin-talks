9094281c5ce87e30d1fb3262d0020e3cd5bc1d74
Ian Brandt <ian@ianbrandt.com>
2020-03-01 10:59:17 -0800

    Another example of adding external components to a module.

--- di-with-koin/src/it/kotlin/org/sdkotlin/koin/it/hello/HelloModuleIT.kt
+++ di-with-koin/src/it/kotlin/org/sdkotlin/koin/it/hello/HelloModuleIT.kt
@@ -1,19 +1,25 @@
 package org.sdkotlin.koin.it.hello
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
+import org.koin.core.error.InstanceCreationException
 import org.koin.core.parameter.parametersOf
 import org.koin.core.qualifier.named
 import org.koin.test.KoinTest
 import org.koin.test.get
 import org.koin.test.inject
+import org.sdkotlin.koin.hello.DECLARED_COMPONENT
+import org.sdkotlin.koin.hello.DECLARED_COMPONENT_CONTAINER
 import org.sdkotlin.koin.hello.ExternalComponent
 import org.sdkotlin.koin.hello.ExternalComponentContainer
 import org.sdkotlin.koin.hello.HelloController
+import org.sdkotlin.koin.hello.REVERSE_INJECTED_COMPONENT
+import org.sdkotlin.koin.hello.REVERSE_INJECTED_COMPONENT_CONTAINER
 import org.sdkotlin.koin.hello.RandomGreetingService
 import org.sdkotlin.koin.hello.helloModule
 
@@ -34,7 +40,7 @@ internal class HelloModuleIT : KoinTest {
 	}
 
 	@Test
-	fun `Integration test the hello module`() {
+	fun `test the hello module`() {
 
 		val response = helloController.sayHello()
 
@@ -42,19 +48,39 @@ internal class HelloModuleIT : KoinTest {
 	}
 
 	@Test
-	fun `Integration test the external entity`() {
+	fun `test declaring an external component`() {
 
 		val expectedValue = "testing"
 
+		assertThatExceptionOfType(InstanceCreationException::class.java).isThrownBy {
+			get<ExternalComponentContainer>(named(DECLARED_COMPONENT_CONTAINER))
+		}
+
+		getKoin().declare(ExternalComponent(expectedValue), named(DECLARED_COMPONENT))
+
+		val externalComponentContainer = get<ExternalComponentContainer>(named(DECLARED_COMPONENT_CONTAINER))
+
+		assertThat(externalComponentContainer.externalComponent.value).isEqualTo(expectedValue)
+	}
+
+	@Test
+	fun `test reverse injecting an external component`() {
+
+		val expectedValue = "testing"
+
+		assertThatExceptionOfType(InstanceCreationException::class.java).isThrownBy {
+			get<ExternalComponentContainer>(named(REVERSE_INJECTED_COMPONENT_CONTAINER))
+		}
+
 		// "Reverse inject" the external component into the Koin module as an
 		// injection parameter for a singleton that is the parameter itself
-		get<ExternalComponent>(named<ExternalComponent>()) {
+		get<ExternalComponent>(named(REVERSE_INJECTED_COMPONENT)) {
 			parametersOf(ExternalComponent(expectedValue))
 		}
 
 		// Get another component from Koin that depends on the external
 		// component having been injected into the module
-		val externalComponentContainer = get<ExternalComponentContainer>(named<ExternalComponentContainer>())
+		val externalComponentContainer = get<ExternalComponentContainer>(named(REVERSE_INJECTED_COMPONENT_CONTAINER))
 
 		assertThat(externalComponentContainer.externalComponent.value).isEqualTo(expectedValue)
 	}
--- di-with-koin/src/main/kotlin/org/sdkotlin/koin/hello/helloModule.kt
+++ di-with-koin/src/main/kotlin/org/sdkotlin/koin/hello/helloModule.kt
@@ -3,6 +3,11 @@ package org.sdkotlin.koin.hello
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
+const val DECLARED_COMPONENT = "declared component"
+const val DECLARED_COMPONENT_CONTAINER = "declared component container"
+const val REVERSE_INJECTED_COMPONENT = "reverse injected component"
+const val REVERSE_INJECTED_COMPONENT_CONTAINER = "reverse injected component container"
+
 val helloModule = module {
 
 	single<GreetingService> { RandomGreetingService() }
@@ -10,7 +15,22 @@ val helloModule = module {
 
 	single<HelloController> { SimpleHelloController(get()) }
 
-	single(named<ExternalComponent>()) { (externalComponent: ExternalComponent) -> externalComponent }
+	// Injection will fail if DECLARED_COMPONENT isn't externally declared prior, e.g. with getKoin().declare(...)
+	single(named(DECLARED_COMPONENT_CONTAINER)) {
+		ExternalComponentContainer(
+				get(named(DECLARED_COMPONENT))
+		)
+	}
 
-	single(named<ExternalComponentContainer>()) { ExternalComponentContainer(get(named<ExternalComponent>())) }
+	// A parameterized component where the parameter ends up being the component itself
+	single(named(REVERSE_INJECTED_COMPONENT)) {
+		(externalComponent: ExternalComponent) -> externalComponent
+	}
+
+	// Injection will fail if REVERSE_INJECTED_COMPONENT isn't reverse injected prior
+	single(named(REVERSE_INJECTED_COMPONENT_CONTAINER)) {
+		ExternalComponentContainer(
+				get(named(REVERSE_INJECTED_COMPONENT))
+		)
+	}
 }
