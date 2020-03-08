f9b796d1059482e7c5da63b4c7ec158bbbd067a1
Ian Brandt <ian@ianbrandt.com>
2020-03-08 15:44:35 -0700

    Improved Gradle Versions Plugin filtering.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,21 +16,17 @@ allprojects {
 	}
 }
 
+fun isNonStable(version: String): Boolean {
+	val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+	val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+	val isStable = stableKeyword || regex.matches(version)
+	return isStable.not()
+}
+
 tasks {
-	named<DependencyUpdatesTask>("dependencyUpdates") {
-		revision = "release"
-		gradleReleaseChannel = CURRENT.id
-		resolutionStrategy {
-			componentSelection {
-				all {
-					val rejected = listOf("alpha", "beta", "rc", "cr", "m", "preview", "b", "ea").any { qualifier ->
-						candidate.version.matches(Regex("(?i).*[.-]$qualifier.*"))
-					}
-					if (rejected) {
-						reject("Release candidate")
-					}
-				}
-			}
+	withType<DependencyUpdatesTask> {
+		rejectVersionIf {
+			isNonStable(candidate.version)
 		}
 	}
 
