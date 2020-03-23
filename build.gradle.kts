84118f2fb85b1b6e952c83f5add2f84a351d3abd
Ian Brandt <ian@ianbrandt.com>
2020-03-23 11:35:33 -0700

    Improved dependency update stable version filtering.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,9 @@ allprojects {
 
 fun isNonStable(version: String): Boolean {
 	val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+	val unstableKeyword = listOf("""M\d+""".toRegex()).any { version.toUpperCase().contains(it) }
 	val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-	val isStable = stableKeyword || regex.matches(version)
+	val isStable = (stableKeyword && !unstableKeyword) || regex.matches(version)
 	return isStable.not()
 }
 
