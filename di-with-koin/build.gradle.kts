12a5d2beac032f992410ea2243418b63ee74ddc3
Ian Brandt <ian@ianbrandt.com>
2020-04-11 14:20:07 -0700

    Upgraded the Gradle Test Sets plugin.

--- di-with-koin/build.gradle.kts
+++ di-with-koin/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 	`kotlin-project`
 	`junit-project`
 	`assertj-project`
-	id("org.unbroken-dome.test-sets") version "3.0.0"
+	id("org.unbroken-dome.test-sets") version "3.0.1"
 }
 
 dependencies {
