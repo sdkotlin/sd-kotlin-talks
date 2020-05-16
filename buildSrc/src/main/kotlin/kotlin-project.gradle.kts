57ecfcbf30be9804defd8fd3b987e24ef5d169ce
Ian Brandt <ian@ianbrandt.com>
2020-05-16 11:06:02 -0700

    Upgraded Kotlin coroutines.

--- buildSrc/src/main/kotlin/kotlin-project.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-project.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 dependencies {
 	implementation(kotlin("stdlib-jdk8"))
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.6")
 }
 
 java {
