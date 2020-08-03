bc90ee54f9f095c0aa25698ea79a82f9eb1bc1c5
Ian Brandt <ian@ianbrandt.com>
2020-08-03 12:10:05 -0700

    Added properties example for Kotlin.

--- kotlin-for-java-devs/src/main/java/org/sdkotlin/intro/java/_06_equality/EqualityInJava.java
+++ kotlin-for-java-devs/src/main/java/org/sdkotlin/intro/java/_06_equality/EqualityInJava.java
@@ -2,7 +2,7 @@ package org.sdkotlin.intro.java._06_equality;
 
 public class EqualityInJava {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 
 		System.out.println("foo" == new String("foo"));
 		System.out.println("foo".equals(new String("foo")));
