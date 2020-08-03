be7f9abd445e54182c28cc7b1da76def7f160bdc
Ian Brandt <ian@ianbrandt.com>
2020-08-03 08:13:09 -0700

    Added placeholders.

---
+++ kotlin-for-java-devs/src/main/kotlin/org/sdkotlin/intro/kotlin/_XX_arrays/ArraysInKotlin.kt
@@ -0,0 +1,3 @@
+package org.sdkotlin.intro.kotlin._XX_arrays
+
+// TODO: Add an example for arrays in Kotlin.
---
+++ kotlin-for-java-devs/src/main/kotlin/org/sdkotlin/intro/kotlin/_XX_collections/CollectionsInKotlin.kt
@@ -0,0 +1,3 @@
+package org.sdkotlin.intro.kotlin._XX_collections
+
+// TODO: Add an example for collections in Kotlin.
