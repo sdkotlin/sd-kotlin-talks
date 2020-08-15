6b9c121088be8572fe1fe32afe1e3159d06f3fe8
Ian Brandt <ian@ianbrandt.com>
2020-08-14 22:58:07 -0700

    Added abstract classes example for Kotlin.

--- kotlin-for-java-devs/src/main/kotlin/org/sdkotlin/intro/kotlin/_19_abstractclasses/AbstractClassesInKotlin.kt
+++ kotlin-for-java-devs/src/main/kotlin/org/sdkotlin/intro/kotlin/_19_abstractclasses/AbstractClassesInKotlin.kt
@@ -1,3 +1,65 @@
 package org.sdkotlin.intro.kotlin._19_abstractclasses
 
-// TODO: Add abstract classes example for Kotlin.
+// Kotlin supports abstract classes and members.
+
+// Abstract classes are implicitly open.
+
+abstract class DinnerTemplate {
+
+	protected open fun cookFood() {
+		println("It's pasta for dinner")
+	}
+
+	// Abstract members are implicitly open as well.
+
+	protected abstract fun serve()
+
+	protected abstract fun enjoy()
+
+	fun haveDinner() {
+		cookFood()
+		serve()
+		enjoy()
+	}
+}
+
+class NiceMeal : DinnerTemplate() {
+
+	override fun serve() {
+		println("Paper plates and plasticware")
+	}
+
+	override fun enjoy() {
+		println("Slrrrp")
+	}
+}
+
+abstract class NotPastaAgainTemplate : DinnerTemplate() {
+
+	// An open member function can be overridden as abstract.
+	abstract override fun cookFood()
+}
+
+class FancyMeal : NotPastaAgainTemplate() {
+
+	override fun cookFood() {
+		println("Chef's special")
+	}
+
+	override fun serve() {
+		println("Fine china and silverware")
+	}
+
+	override fun enjoy() {
+		println("Bon appetit")
+	}
+}
+
+fun main() {
+
+	val niceMeal = NiceMeal()
+	val fancyMeal = FancyMeal()
+
+	niceMeal.haveDinner()
+	fancyMeal.haveDinner()
+}
