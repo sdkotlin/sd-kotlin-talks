85c4abf7079b4ea3f7fd1c2d1d77c7ef079c5b89
Ian Brandt <ian@ianbrandt.com>
2020-08-17 11:17:41 -0700

    Improved extension functions example.

--- kotlin-for-java-devs/src/main/kotlin/org/sdkotlin/intro/kotlin/_08_3_extensionfunctions/ExtensionFunctionsInKotlin.kt
+++ kotlin-for-java-devs/src/main/kotlin/org/sdkotlin/intro/kotlin/_08_3_extensionfunctions/ExtensionFunctionsInKotlin.kt
@@ -1,9 +1,25 @@
 package org.sdkotlin.intro.kotlin._08_3_extensionfunctions
 
-// Kotlin allows us to add functions to extend an existing class.
-
-fun String.shuffled() = this.toList().shuffled().joinToString(separator = "")
-
 fun main() {
+
+	// Kotlin allows us to add functions to an existing class.
+
+	fun String.shuffled() = this.toList().shuffled().joinToString(separator = "")
+
 	println("Alphabet".shuffled())
+
+	// Extension functions are not polymorphic.
+
+	abstract class Animal
+
+	class Lion : Animal()
+
+	fun Animal.name() = "Animal"
+	fun Lion.name() = "Simba"
+
+	val genericLion: Animal = Lion()
+	val specificLion = Lion()
+
+	println("Lion as Animal's name: ${genericLion.name()}")
+	println("Lion as Lion's name: ${specificLion.name()}")
 }
