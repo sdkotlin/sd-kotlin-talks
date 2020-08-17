37ce5bc6064f1f9fbcf152c29efd6e5e37c701c6
Ian Brandt <ian@ianbrandt.com>
2020-08-17 12:11:07 -0700

    Added infix functions example.

--- kotlin-for-java-devs/src/main/kotlin/org/sdkotlin/intro/kotlin/_08_5_infixfunctions/InfixFunctionsInKotlin.kt
+++ kotlin-for-java-devs/src/main/kotlin/org/sdkotlin/intro/kotlin/_08_5_infixfunctions/InfixFunctionsInKotlin.kt
@@ -1,3 +1,23 @@
 package org.sdkotlin.intro.kotlin._08_5_infixfunctions
 
-// TODO: Add infix functions example for Kotlin.
+import kotlin.random.Random
+
+// Kotlin supports defining member and extension functions that can be called
+// using infix notation.
+
+infix fun Int.plusOrMinus(other: Int) =
+		if (Random.nextBoolean()) {
+			this + other
+		} else {
+			this - other
+		}
+
+// Note that the function identifier naming constraints in Kotlin prevent us
+// from defining new symbolic operators.
+
+fun main() {
+
+	val result = 1 plusOrMinus 2
+
+	println("1 ± 2: $result")
+}
