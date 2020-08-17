d0637294a3a0e93eec09f72387255b8130a704da
Ian Brandt <ian@ianbrandt.com>
2020-08-17 10:49:53 -0700

    Added interfaces in Kotlin example.

--- kotlin-for-java-devs/src/main/kotlin/org/sdkotlin/intro/kotlin/_20_interfaces/InterfacesInKotlin.kt
+++ kotlin-for-java-devs/src/main/kotlin/org/sdkotlin/intro/kotlin/_20_interfaces/InterfacesInKotlin.kt
@@ -1,3 +1,63 @@
 package org.sdkotlin.intro.kotlin._20_interfaces
 
-// TODO: Add interfaces example for Kotlin.
+// Interfaces in Kotlin work mostly as you'd expect coming from Java, with a
+// few differences when it comes to properties and constants.
+
+interface Shape {
+
+	// They can declare abstract functions.
+
+	fun draw()
+
+	// They can declare default functions.
+
+	fun describe() = println("I'm shapely.")
+
+	// They can declare abstract properties.
+
+	val name: String
+
+	// Constants are not supported.
+
+	//const val CONSTANT = false
+
+	// Interfaces can't store state, so any non-abstract properties must be
+	// computed.
+
+	val abstractName
+		get() = "Shape"
+
+	// They can be var given a side-effect setter. Maybe this could be
+	// exploited when creating internal DSLs.
+
+	var hmm
+		get() = "🤔"
+		set(value) = println("Hmm... $value")
+}
+
+class Circle(override val name: String) : Shape {
+
+	override fun draw() {
+		println("O")
+	}
+
+	// Default methods can be overridden.
+
+	override fun describe() = println("I'm no square.")
+}
+
+fun main() {
+	val circle = Circle("Roundy")
+
+	print("circle.draw(): ")
+	circle.draw()
+
+	print("circle.describe(): ")
+	circle.describe()
+
+	println("circle.name: ${circle.name}")
+	println("circle.abstractName: ${circle.abstractName}")
+	println("circle.hmm: ${circle.hmm}")
+	print("circle.hmm = ")
+	circle.hmm = "For DSLs maybe?"
+}
