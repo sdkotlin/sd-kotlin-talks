8e6ca4bfbfcc1d8308cae45e0e77579db57db5bd
Ian Brandt <ian@ianbrandt.com>
2020-09-09 10:47:12 -0700

    Started sequences example (hit https://youtrack.jetbrains.com/issue/IDEA-200104).

--- kotlin-for-java-devs/src/main/kotlin/org/sdkotlin/intro/kotlin/_27_sequences/SequencesInKotlin.kt
+++ kotlin-for-java-devs/src/main/kotlin/org/sdkotlin/intro/kotlin/_27_sequences/SequencesInKotlin.kt
@@ -1,3 +1,22 @@
 package org.sdkotlin.intro.kotlin._27_sequences
 
-// TODO: Add sequences example for Kotlin.
+// Kotlin has sequences for lazy collection stream operations, very similar to
+// Java 8 streams.
+
+fun main() {
+
+	// Let's take a big list of random numbers between 1-100.
+
+	val listOfInts = List(1000) { (1..100).random() }
+
+	// Say we want to find the first even number greater than 50.
+
+	val bigEven = listOfInts.asSequence()
+			.filter { println("Filtering $it"); it.isEven() }
+			.find { println("Finding $it"); it > 50 }
+			?.toString() ?: "Not Found"
+
+	println("First even number over 50: $bigEven")
+}
+
+fun Int.isEven() = this % 2 == 0
