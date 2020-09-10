63aa75d9cad689a73aa9ddf92ce042586cc1c3b5
Ian Brandt <ian@ianbrandt.com>
2020-09-10 13:35:01 -0700

    Improved sequences example.

--- kotlin-for-java-devs/src/main/kotlin/org/sdkotlin/intro/kotlin/_27_sequences/SequencesInKotlin.kt
+++ kotlin-for-java-devs/src/main/kotlin/org/sdkotlin/intro/kotlin/_27_sequences/SequencesInKotlin.kt
@@ -7,7 +7,7 @@ fun main() {
 
 	// Let's take a big list of random numbers between 1-100.
 
-	val listOfInts = List(1000) { (1..100).random() }
+	val listOfRandomInts = List(1000) { (1..100).random() }
 
 	// Say we want to find the first even number greater than 50.
 
@@ -17,7 +17,7 @@ fun main() {
 	var collectionFilters = 0
 	var collectionFinds = 0
 
-	val collectionBigEven = listOfInts
+	val collectionBigEven = listOfRandomInts
 			.filter { collectionFilters++; it.isEven() }
 			.find { collectionFinds++; it > 50 }
 
@@ -29,12 +29,60 @@ fun main() {
 	var sequenceFilters = 0
 	var sequenceFinds = 0
 
-	val sequenceBigEven = listOfInts.asSequence()
+	val sequenceBigEven = listOfRandomInts.asSequence()
 			.filter { sequenceFilters++; it.isEven() }
 			.find { sequenceFinds++; it > 50 }
 
 	println("$sequenceBigEven found with $sequenceFilters filter and " +
 			"$sequenceFinds find sequence operations.")
+
+	// There are some benefits to using the Kotlin sequence APIs over Java 8
+	// streams:
+	//     * Additional null safety
+	//     * Leaner, more Kotlin-like API
+	//     * Multiplatform
+	//     * Sequences can be reused, even the intermediaries
+
+	val streamBigEven = listOfRandomInts.stream()
+			.filter { it.isEven() }
+			// Additional intermediate operation instead of Kotlin's terminal
+			// `.find(Predicate)`
+			.filter { it > 100 }
+			.findFirst() // Returns an `Optional`
+			// The compiler doesn't force us to default the value with
+			// `orElse()` instead of `get()`
+			.orElse(null)
+
+	// And even when using `Optional.orElse()`, we still end up with a platform
+	// type.
+
+	try {
+		val nope = streamBigEven + 1
+	} catch (e: NullPointerException) {
+		println("streamBigEven + 1: KABLAMO!")
+	}
+
+	val sequenceOfRandomInts = listOfRandomInts.asSequence()
+
+	// We can reuse a sequence, even after calling a terminal operation on it.
+
+	println("sequenceOfRandomInts.average() ${sequenceOfRandomInts.average()}")
+	println("sequenceOfRandomInts.sum() ${sequenceOfRandomInts.sum()}")
+
+	// We can reuse intermediary sequences.
+
+	val sequenceOfRandomEvens = sequenceOfRandomInts.filter { it.isEven() }
+
+	val sequenceOfRandomOdds = sequenceOfRandomInts.filter { !it.isEven() }
+
+	println("sequenceOfRandomEvens.average() ${sequenceOfRandomEvens.average()}")
+	println("sequenceOfRandomOdds.average() ${sequenceOfRandomOdds.average()}")
+
+	// Kotlin doesn't have parallel sequences as would be analogous to Java's
+	// parallel streams.
+	//
+	// Parallel Flow processing is being considered for this use case:
+	// https://github.com/Kotlin/kotlinx.coroutines/issues/1147
 }
 
 fun Int.isEven() = this % 2 == 0
