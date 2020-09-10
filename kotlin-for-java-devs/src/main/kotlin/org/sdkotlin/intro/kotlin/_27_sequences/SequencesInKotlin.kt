5cdab1c2b28d19b8aa2dee373f14ae5ef8b476d5
Ian Brandt <ian@ianbrandt.com>
2020-09-10 14:52:36 -0700

    Improved sequences example.

--- kotlin-for-java-devs/src/main/kotlin/org/sdkotlin/intro/kotlin/_27_sequences/SequencesInKotlin.kt
+++ kotlin-for-java-devs/src/main/kotlin/org/sdkotlin/intro/kotlin/_27_sequences/SequencesInKotlin.kt
@@ -36,6 +36,47 @@ fun main() {
 	println("$sequenceBigEven found with $sequenceFilters filter and " +
 			"$sequenceFinds find sequence operations.")
 
+	// If you're not starting with a collection, sequences can be created
+	// directly via a factory function.
+
+	val sequenceOfFlightlessBirds
+			= sequenceOf("Penguin", "Ostrich", "Kiwi", "Emu")
+
+	println("Flightless birds: ${sequenceOfFlightlessBirds.sorted().toList()}")
+
+	// You can create a sequence from an existing function or lambda. The
+	// sequence ends if and when the function returns null.
+
+	val moreRandomInts = generateSequence {
+		(0..10).random().takeIf { it != 0 }
+	}
+
+	println("Some random positive ints: ${moreRandomInts.toList()}")
+
+	// You can seed the generator, and then the function gets the previous
+	// value as an argument.
+
+	val infiniteInts = generateSequence(0) { it + 1 }
+
+	// This can result in an infinite loop, so be sure to use a limiting
+	// operation on such a sequence.
+
+	println("Will it ever end? ${infiniteInts.take(5).toList()}")
+
+	// We haven't covered coroutines yet, but they're like lightweight threads.
+	// There is a `sequence` generator/`yield` suspending function for creating
+	// non-blocking sequence generators.
+
+	val fibonacciSequence = sequence {
+		var terms = Pair(0, 1)
+		while (true) {
+			yield(terms.first)
+			terms = Pair(terms.second, terms.first + terms.second)
+		}
+	}
+
+	println("The Fibonacci sequance: ${fibonacciSequence.take(10).toList()}...")
+
 	// There are some benefits to using the Kotlin sequence APIs over Java 8
 	// streams:
 	//     * Additional null safety
@@ -78,11 +119,21 @@ fun main() {
 	println("sequenceOfRandomEvens.average() ${sequenceOfRandomEvens.average()}")
 	println("sequenceOfRandomOdds.average() ${sequenceOfRandomOdds.average()}")
 
-	// Kotlin doesn't have parallel sequences as would be analogous to Java's
-	// parallel streams.
+	// Java 8 streams do have a couple advantages over Kotlin sequences:
+	//     * Kotlin doesn't have parallel sequences as would be analogous to
+	//       Java's parallel streams.
 	//
-	// Parallel Flow processing is being considered for this use case:
-	// https://github.com/Kotlin/kotlinx.coroutines/issues/1147
+	//       Parallel Flow processing is being considered for this use case:
+	//       https://github.com/Kotlin/kotlinx.coroutines/issues/1147
+	//
+	//     * Java has primitive type streams to potentially minimize boxing.
+	//
+	// In practice, both parallel streams and primitive type streams have
+	// caveats, and sequences have some mitigating options. These may reduce
+	// or nullify any performance advantages streams might seem to possess over
+	// Kotlin sequences. For highly performance sensitive code, neither streams
+	// nor sequences may be an effective choice. The usual "always benchmark
+	// your use case" warning applies.
 }
 
 fun Int.isEven() = this % 2 == 0
