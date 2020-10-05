2381d9c25f96aca54c8ca6bbc332618259683d2d
Ian Brandt <ian@ianbrandt.com>
2020-10-05 10:33:41 -0700

    Improved delegation example for Kotlin.

--- kotlin-for-java-devs/src/main/kotlin/org/sdkotlin/intro/kotlin/_32_delegation/DelegationInKotlin.kt
+++ kotlin-for-java-devs/src/main/kotlin/org/sdkotlin/intro/kotlin/_32_delegation/DelegationInKotlin.kt
@@ -1,5 +1,14 @@
 package org.sdkotlin.intro.kotlin._32_delegation
 
+import java.awt.Color
+import java.time.LocalDateTime
+import kotlin.properties.Delegates.notNull
+import kotlin.properties.Delegates.observable
+import kotlin.properties.Delegates.vetoable
+import kotlin.properties.ReadOnlyProperty
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
 // Kotlin has built-in support for the Decorator pattern.
 
 // Say we want a list that reports when items are retrieved. Rather than
@@ -49,6 +58,160 @@ fun `with delegates`() {
 	// otherwise final classes, and can be composed into aggregates.
 }
 
+// Kotlin also supports delegation for the access and assignment of
+// properties. This is useful if you want to decorate properties with
+// additional or dynamic behavior in a reusable way.
+
+// Kotlin comes with a few useful property delegates (and factory methods
+// for them) in the standard library.
+
+val timestamped by lazy {
+	println("I'm computed when first accessed!")
+	LocalDateTime.now()
+}
+
+fun `with the Lazy property delegate`() {
+
+	println("timestamped: $timestamped")
+	println("timestamped: $timestamped")
+}
+
+// The Observable property delegate accepts a callback for property changes.
+
+var observed by observable(1) { property, oldValue, newValue ->
+	println("${property.name}: changing from $oldValue to $newValue")
+}
+
+fun `with the Observable property delegate`() {
+
+	println("observed: $observed")
+	observed = 2
+}
+
+// The Vetoable property delegate can block assignment with a callback that
+// returns false.
+
+var vetoed by vetoable(Color.BLUE) { property, oldValue, newValue ->
+	println("${property.name}: ALWAYS $oldValue!")
+	newValue == oldValue
+}
+
+fun `with the Vetoable property delegate`() {
+
+	vetoed = Color.GREEN
+	println("vetoed: $vetoed")
+}
+
+// The NotNull property delegate allows for late initialization that isn't null.
+
+var notNull: String by notNull()
+
+fun `with the NotNull property delegate`() {
+
+	try {
+		println(notNull)
+	} catch (e: IllegalStateException) {
+		println(e)
+	}
+
+	notNull = "Late initialized!"
+
+	println("notNull: $notNull")
+}
+
+// We can delegate the storage of a classes properties in a backing map.
+
+class Settings(val settingsMap: Map<String, Any>) {
+	val host: String by settingsMap
+	val port: Int by settingsMap
+}
+
+fun `with the map-backed property delegate`() {
+
+	val settings = Settings(mapOf("host" to "localhost", "port" to 1025))
+
+	println("settings.host: ${settings.host}")
+	println("settings.port: ${settings.port}")
+
+	println("settingsMap: ${settings.settingsMap}")
+}
+
+// This works for mutable properties as well.
+
+class Stats(val statsMap: MutableMap<String, Any?>) {
+	var reads: Int by statsMap
+	var writes: Int by statsMap
+}
+
+fun `with the mutable map-backed property delegate`() {
+
+	val stats = Stats(mutableMapOf("reads" to 0, "writes" to 0))
+
+	stats.reads++
+	stats.statsMap["writes"] = 2
+
+	println("stats.reads: ${stats.reads}")
+	println("stats.writes: ${stats.writes}")
+
+	println("statsMap: ${stats.statsMap}")
+}
+
+// We can create our own `val` property delegate by implementing
+// `ReadOnlyProperty`.
+
+class SpyingVal<V>(private val value: V) : ReadOnlyProperty<Any?, V> {
+
+	override fun getValue(thisRef: Any?, property: KProperty<*>): V {
+		println("Getting '${property.name}' from $thisRef as '$value'.")
+		return value
+	}
+}
+
+// And the same for a `var` property with `ReadWriteProperty`.
+
+// Implementing these interfaces is actually optional, but it helps for getting
+// the signatures right.
+
+class SpyingVar<V>(private var value: V) : ReadWriteProperty<Any?, V> {
+
+	override operator fun getValue(thisRef: Any?, property: KProperty<*>): V {
+		println("Getting '${property.name}' from $thisRef as '$value'.")
+		return value
+	}
+
+	override operator fun setValue(
+		thisRef: Any?,
+		property: KProperty<*>,
+		value: V
+	) {
+		this.value = value
+		println("Changing '${property.name}' in $thisRef to '$value'.")
+	}
+}
+
+class Paranoid {
+	val someonesWatchingMe by SpyingVal("Paranoid")
+	var youAndMeBoth by SpyingVar("Also Paranoid")
+}
+
+fun `creating property delegates`() {
+
+	val paranoid = Paranoid()
+
+	println("someonesWatchingMe: ${paranoid.someonesWatchingMe}")
+
+	paranoid.youAndMeBoth = "Double Paranoid"
+
+	println("youAndMeBoth: ${paranoid.youAndMeBoth}")
+}
+
 fun main() {
 	`with delegates`()
+	`with the Lazy property delegate`()
+	`with the Observable property delegate`()
+	`with the Vetoable property delegate`()
+	`with the NotNull property delegate`()
+	`with the map-backed property delegate`()
+	`with the mutable map-backed property delegate`()
+	`creating property delegates`()
 }
