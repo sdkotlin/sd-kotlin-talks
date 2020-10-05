64bd4c2119d92faae2dc8595b145d91c291d4448
Ian Brandt <ian@ianbrandt.com>
2020-10-05 11:03:07 -0700

    Improved static factory function examples.

--- effective-kotlin/src/main/kotlin/org/sdkotlin/meetup/effectivejava/item1/ConsiderStaticFactoryMethods.kt
+++ effective-kotlin/src/main/kotlin/org/sdkotlin/meetup/effectivejava/item1/ConsiderStaticFactoryMethods.kt
@@ -19,42 +19,26 @@ Item 1: Consider static factory methods instead of constructors
  */
 
 // Kotlin companion object functions, companion extension functions, and
-// top-level functions can be used as powerful alternatives to Java's static
-// factory methods
+// top-level functions can be used as alternatives to Java's static factory
+// methods.
 
-fun main() {
+fun `with Java static factory functions`() {
 
-	// Java static factory usage
 	val charset = Charset.forName("ASCII")
+
 	println("Java charset is \"${charset.name()}\"")
-
-	// Kotlin companion object factory function usage
-	val alienCharset =
-			AlienCharset.forName("Klingon")
-	println("Alien charset is \"${alienCharset.name()}\"")
-
-	// Kotlin companion object extension factory function usage
-	val grootSays = String.valueOf(Groot())
-	println("Groot says, \"$grootSays\"")
-
-	// Kotlin top-level factory function usage
-	val months = sortedMapOf(2 to FEBRUARY, 1 to JANUARY,
-			3 to MARCH)
-	println("The months in order are $months")
 }
 
-// Companion object factory function
+// In Kotlin a companion object function simulates a Java static function.
+
 abstract class AlienCharset {
-
 	companion object {
-
 		fun forName(charsetName: String) = when (charsetName) {
 			"Klingon" -> KlingonCharset()
 			"Alienese" -> AlieneseCharset()
 			else -> throw IllegalArgumentException("Unknown character set")
 		}
 	}
-
 	abstract fun name(): String
 }
 
@@ -66,13 +50,44 @@ class AlieneseCharset : AlienCharset() {
 	override fun name() = "Alienese"
 }
 
-// Companion object extension factory function
+fun `with Kotlin companion object factory functions`() {
+
+	val alienCharset = AlienCharset.forName("Klingon")
+
+	println("Alien charset is \"${alienCharset.name()}\"")
+}
+
+// We can add such a static factory as a companion object extension function.
+
 fun String.Companion.valueOf(groot: Groot) = groot.toString()
 
 class Groot {
 	override fun toString() = "I am Groot"
 }
 
-// Top-level factory function
+fun `with Kotlin companion object extension factory functions`() {
+
+	val grootSays = String.valueOf(Groot())
+
+	println("Groot says, \"$grootSays\"")
+}
+
+// We can also create top-level factory functions.
+
 fun <K, V> sortedMapOf(vararg pairs: Pair<K, V>): SortedMap<K, V> =
 		TreeMap<K, V>().apply { putAll(pairs) }
+
+fun `with Kotlin top-level factory functions`() {
+
+	val months = sortedMapOf(2 to FEBRUARY, 1 to JANUARY, 3 to MARCH)
+
+	println("The months in order are $months")
+}
+
+fun main() {
+
+	`with Java static factory functions`()
+	`with Kotlin companion object factory functions`()
+	`with Kotlin companion object extension factory functions`()
+	`with Kotlin top-level factory functions`()
+}
