0d15543eda467917fbe861a03a788ed3ad8e5224
Ian Brandt <ian@ianbrandt.com>
2020-11-24 21:24:36 -0800

    Upgraded Mockito.

--- buildSrc/src/main/kotlin/mockito-kotlin-project.gradle.kts
+++ buildSrc/src/main/kotlin/mockito-kotlin-project.gradle.kts
@@ -5,5 +5,5 @@ plugins {
 
 dependencies {
 	testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
-	testImplementation("org.mockito:mockito-core:3.6.0")
+	testImplementation("org.mockito:mockito-core:3.6.28")
 }
