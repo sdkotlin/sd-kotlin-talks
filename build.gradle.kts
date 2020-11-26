e3c44f789fcd53d3378ac87744467608cb7c6f8c
Ian Brandt <ian@ianbrandt.com>
2020-11-26 08:50:09 -0800

    Upgraded Gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ tasks {
 
 	named<Wrapper>("wrapper") {
 		// https://github.com/gradle/gradle/issues/12660
-		gradleVersion = "6.8-milestone-3"
+		gradleVersion = "6.8-rc-1"
 		distributionType = ALL
 	}
 }
