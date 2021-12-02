0c599c7df7470e11fc0510dce6e098c7f1a954a2
Ian Brandt <ian@ianbrandt.com>
2021-12-02 14:08:51 -0800

    Upgraded MockK.

--- buildSrc/src/main/kotlin/mockk-project.gradle.kts
+++ buildSrc/src/main/kotlin/mockk-project.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 dependencies {
 
-	val mockkVersion = "1.12.0"
+	val mockkVersion = "1.12.1"
 
 	testImplementation("io.mockk:mockk:$mockkVersion")
 	testImplementation("io.mockk:mockk-dsl-jvm:$mockkVersion")
