d38b81ae276092f07d45fcdb8ee82399d4f4c845
Ian Brandt <ian@ianbrandt.com>
2022-11-02 11:01:40 -0700

    Platform code cleanup.

--- platforms/app-platform/build.gradle.kts
+++ platforms/app-platform/build.gradle.kts
@@ -5,13 +5,11 @@ plugins {
 group = "org.sdkotlin.platforms"
 
 dependencies {
-
 	constraints {
 		api(libs.bundles.arrow.jvm)
 		api(libs.jetbrains.annotations)
 		api(libs.bundles.koin.jvm)
 		api(libs.bundles.kotlinx.coroutines.jvm)
-		api(libs.bundles.kotlinx.dl)
 		api(libs.bundles.kotlinx.dl)
 		api(libs.org.json)
 		api(libs.slf4j.simple)
