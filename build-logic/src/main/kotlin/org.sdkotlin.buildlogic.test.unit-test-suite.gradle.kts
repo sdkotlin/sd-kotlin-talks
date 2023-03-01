2594321a5a8dd9aa5d08427b0934f4274ce39c85
Ian Brandt <ian.r.brandt@saic.com>
2023-03-01 09:16:21 -0800

    Added dependency version code comments.

--- build-logic/src/main/kotlin/org.sdkotlin.buildlogic.test.unit-test-suite.gradle.kts
+++ build-logic/src/main/kotlin/org.sdkotlin.buildlogic.test.unit-test-suite.gradle.kts
@@ -16,8 +16,8 @@ testing {
 
 			// Version catalog not available in precompiled script plugins:
 			// https://github.com/gradle/gradle/issues/15383
-			//val junitVersion = libs.version.junit
-			val junitVersion = "5.9.2"
+			//val junitVersion = libs.version.junit.get()
+			val junitVersion = "5.9.2" // Duplicated in libs.versions.toml.
 
 			useJUnitJupiter(junitVersion)
 
