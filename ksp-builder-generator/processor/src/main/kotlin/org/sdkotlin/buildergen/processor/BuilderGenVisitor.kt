2ed11b0d6ca475c3a0896eaa1ae217f6e8b039a2
Ian Brandt <ian@ianbrandt.com>
2023-11-13 00:15:04 -0800

    Added mutable properties to builder.

--- ksp-builder-generator/processor/src/main/kotlin/org/sdkotlin/buildergen/processor/BuilderGenVisitor.kt
+++ ksp-builder-generator/processor/src/main/kotlin/org/sdkotlin/buildergen/processor/BuilderGenVisitor.kt
@@ -4,15 +4,20 @@ import com.google.devtools.ksp.processing.CodeGenerator
 import com.google.devtools.ksp.processing.KSPLogger
 import com.google.devtools.ksp.symbol.KSClassDeclaration
 import com.google.devtools.ksp.symbol.KSFunctionDeclaration
+import com.google.devtools.ksp.symbol.KSName
+import com.google.devtools.ksp.symbol.KSTypeReference
+import com.google.devtools.ksp.symbol.KSValueParameter
 import com.google.devtools.ksp.symbol.KSVisitorVoid
 import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.KModifier
 import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
+import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeSpec
 import com.squareup.kotlinpoet.asClassName
 import com.squareup.kotlinpoet.ksp.toClassName
+import com.squareup.kotlinpoet.ksp.toTypeName
 import com.squareup.kotlinpoet.ksp.writeTo
 import org.sdkotlin.buildergen.api.builder.Builder
 
@@ -49,26 +54,55 @@ internal class BuilderGenVisitor(
 		val entityBuilderKPClassName =
 			ClassName(entityPackageName, entityBuilderClassName)
 
-		val builderInterfaceKPClassName = Builder::class.asClassName()
+		val builderInterfaceKPClassName: ClassName =
+			Builder::class.asClassName()
+
+		val entityClassTypeSpecBuilder: TypeSpec.Builder =
+			TypeSpec.classBuilder(entityBuilderKPClassName)
+
+		entityClassTypeSpecBuilder
+			.addSuperinterface(
+				builderInterfaceKPClassName
+					.parameterizedBy(entityKPClassName)
+			)
+
+		function.parameters.forEach { parameter: KSValueParameter ->
+
+			val parameterName: KSName = parameter.name!!
+			val parameterType: KSTypeReference = parameter.type
+
+			entityClassTypeSpecBuilder.addProperty(
+				PropertySpec.builder(
+					parameterName.asString(),
+					parameterType.toTypeName()
+				)
+					.mutable(true)
+					.addModifiers(KModifier.LATEINIT)
+					.build()
+			)
+		}
+
+		entityClassTypeSpecBuilder.addFunction(
+			FunSpec.builder("build")
+				.addModifiers(KModifier.OVERRIDE)
+				.returns(entityKPClassName)
+				// TODO: Complete builder implementation
+				.addStatement("TODO()")
+				.build()
+		)
+
+		val entityBuilderTypeSpec: TypeSpec =
+			entityClassTypeSpecBuilder.build()
+
+		val entityBuilderFileSpecBuilder: FileSpec.Builder =
+			FileSpec.builder(entityBuilderKPClassName)
+
+		entityBuilderFileSpecBuilder.addType(
+			entityBuilderTypeSpec
+		)
 
 		val entityBuilderFileSpec: FileSpec =
-			FileSpec.builder(entityBuilderKPClassName)
-				.addType(
-					TypeSpec.classBuilder(entityBuilderKPClassName)
-						.addSuperinterface(
-							builderInterfaceKPClassName
-								.parameterizedBy(entityKPClassName)
-						)
-						// TODO: Complete builder implementation
-						.addFunction(
-							FunSpec.builder("build")
-								.addModifiers(KModifier.OVERRIDE)
-								.returns(entityKPClassName)
-								.addStatement("TODO()")
-								.build()
-						)
-						.build()
-				)
+			entityBuilderFileSpecBuilder
 				.build()
 
 		entityBuilderFileSpec.writeTo(codeGenerator, aggregating = false)
