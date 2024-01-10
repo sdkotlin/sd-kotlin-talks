bb7e573abf8d4af59556dc9fdcb9efd249f05de2
Ian Brandt <ian@ianbrandt.com>
2024-01-10 10:47:23 -0800

    Reverted to Gradle's default test logging configuration.

--- build-logic/src/main/kotlin/org.sdkotlin.buildlogic.test.unit-test-suite.gradle.kts
+++ build-logic/src/main/kotlin/org.sdkotlin.buildlogic.test.unit-test-suite.gradle.kts
@@ -1,9 +1,3 @@
-import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
-import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
-import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
-import org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR
-import org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_OUT
-
 plugins {
 	id("java-library")
 	id("jvm-test-suite")
@@ -24,23 +18,6 @@ testing {
 					.get().preferredVersion
 
 				useJUnitJupiter(junitVersion)
-
-				targets {
-					all {
-						testTask.configure {
-							testLogging {
-								showStandardStreams = true
-								events(
-									PASSED,
-									SKIPPED,
-									FAILED,
-									STANDARD_OUT,
-									STANDARD_ERROR
-								)
-							}
-						}
-					}
-				}
 			}
 		}
 	}
