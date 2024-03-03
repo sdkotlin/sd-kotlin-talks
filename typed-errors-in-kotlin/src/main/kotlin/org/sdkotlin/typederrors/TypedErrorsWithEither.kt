4c2f1b46d7b7f9e8d49914d423e8da8132ac425a
Ian Brandt <ian@ianbrandt.com>
2024-03-02 18:08:39 -0800

    Improved typed errors example.

--- typed-errors-in-kotlin/src/main/kotlin/org/sdkotlin/typederrors/TypedErrorsWithEither.kt
+++ typed-errors-in-kotlin/src/main/kotlin/org/sdkotlin/typederrors/TypedErrorsWithEither.kt
@@ -1,6 +1,7 @@
 package org.sdkotlin.typederrors
 
 import arrow.core.Either
+import arrow.core.flatMap
 import arrow.core.left
 import arrow.core.raise.either
 import arrow.core.right
@@ -21,7 +22,11 @@ sealed interface Error {
 	data object BadFruitError : Error
 }
 
+/**
+ * A fluent, mutation-free builder where some add methods may return errors.
+ */
 interface FruitBasketBuilder {
+
 	fun addApple(apple: Apple): Either<Error, FruitBasket>
 
 	fun addBanana(banana: Banana, microSievertsLimit: Double):
@@ -32,9 +37,17 @@ interface FruitBasketBuilder {
 	fun addOrange(orange: Orange): FruitBasket
 }
 
+/**
+ * The type is also its own builder.
+ */
 interface FruitBasket : FruitBasketBuilder {
 
+	/**
+	 * The companion object implements the builder interface to bootstrap
+	 * the fluent add chain.
+	 */
 	companion object : FruitBasketBuilder {
+
 		override fun addApple(apple: Apple): Either<Error, FruitBasket> =
 			FruitBasketImpl().addApple(apple)
 
@@ -61,6 +74,8 @@ interface FruitBasket : FruitBasketBuilder {
 			if (apple.hasWorm) {
 				BadFruitError.left()
 			} else {
+				// All adds return an appended copy rather than mutate the
+				// original instance.
 				FruitBasketImpl(fruit + apple).right()
 			}
 
@@ -86,32 +101,113 @@ interface FruitBasket : FruitBasketBuilder {
 	}
 }
 
+fun goGroceryShoppingWithNestedFlatMap(): Either<Error, FruitBasket> {
+
+	val isTuesday = true
+	val microSievertsLimit = 5.0
+
+	// Needs to be a `var` since we can't chain iteration.
+	var fruitBasket: Either<Error, FruitBasket>
+
+	fruitBasket = FruitBasket.addApple(Apple(hasWorm = false))
+		.flatMap {
+			it.addBanana(Banana(microSieverts = 1.1), microSievertsLimit)
+				.flatMap {
+					// 'it' is shadowed when nesting flatMap.
+					it.addBanana(
+						Banana(microSieverts = 2.2),
+						microSievertsLimit
+					).flatMap {
+						// Conditional flatMap
+						if (isTuesday) {
+							it.addGrapes(Grapes(moreLikeRaisins = false))
+						} else {
+							it.right()
+						}
+					}
+				}
+		}
+
+	// Iterative flatMap.
+	// There doesn't seem to be any way to perform iterative nested flatMaps,
+	// so we break out of the nesting and chain instead.
+	repeat(3) {
+		fruitBasket = fruitBasket.flatMap {
+			it.addOrange(Orange).right()
+		}
+	}
+
+	return fruitBasket
+}
+
+fun goGroceryShoppingWithChainedFlatMap(): Either<Error, FruitBasket> {
+
+	val isTuesday = true
+	val microSievertsLimit = 5.0
+
+	// Needs to be a `var` since we can't chain iteration.
+	var fruitBasket: Either<Error, FruitBasket>
+
+	fruitBasket = FruitBasket.addApple(Apple(hasWorm = false))
+		.flatMap {
+			it.addBanana(Banana(microSieverts = 1.1), microSievertsLimit)
+		}.flatMap {
+			it.addBanana(Banana(microSieverts = 2.2), microSievertsLimit)
+		}.flatMap {
+			// Conditional flatMap
+			if (isTuesday) {
+				it.addGrapes(Grapes(moreLikeRaisins = false))
+			} else {
+				it.right()
+			}
+		}
+
+	// Iterative flatMap.
+	// There doesn't seem to be any way to perform iterative flatMaps without
+	// breaking the chain.
+	repeat(3) {
+		fruitBasket = fruitBasket.flatMap {
+			it.addOrange(Orange).right()
+		}
+	}
+
+	return fruitBasket
+}
+
 fun goGroceryShopping(): Either<Error, FruitBasket> =
-	either {
+	either { // Using the builder with `.bind()` (i.e. "for-comprehension").
 
 		val isTuesday = true
 		val microSievertsLimit = 5.0
+
+		// Needs to be a `var` since we can't chain iteration, and it seems to
+		// be the most natural way to do conditional adds.
 		var fruitBasket: FruitBasket
 
-		fruitBasket = FruitBasket.addApple(Apple(hasWorm = false)).bind()
-
-		fruitBasket = fruitBasket
+		// Can chain with `bind`.
+		fruitBasket = FruitBasket
+			.addApple(Apple(hasWorm = false)).bind()
 			.addBanana(Banana(microSieverts = 1.0), microSievertsLimit).bind()
 			.addBanana(Banana(microSieverts = 2.2), microSievertsLimit).bind()
 
+		// Conditional adds via breaking the chain.
 		if (isTuesday) {
 			fruitBasket =
 				fruitBasket.addGrapes(Grapes(moreLikeRaisins = false)).bind()
 		}
 
+		// Iterative adds via breaking the chain.
 		repeat(3) {
 			fruitBasket = fruitBasket.addOrange(Orange)
 		}
 
+		// Yield the "Right" value.
 		fruitBasket
 	}
 
 fun main() {
 
+	println(goGroceryShoppingWithNestedFlatMap())
+	println(goGroceryShoppingWithChainedFlatMap())
 	println(goGroceryShopping())
 }
