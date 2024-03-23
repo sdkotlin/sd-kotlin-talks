e99dab00565bda0ab43d0d101c66fbd1b2bde9f6
Ian Brandt <ian@ianbrandt.com>
2024-03-23 13:12:25 -0700

    Added configuration for publishing Gradle Build Scans from CI builds.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,7 @@ pluginManagement {
 }
 
 plugins {
+	id("com.gradle.enterprise") version "3.16.2"
 	id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
@@ -20,6 +21,16 @@ dependencyResolutionManagement {
 
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+gradleEnterprise {
+	if (System.getenv("CI") != null) {
+		buildScan {
+			publishAlways()
+			termsOfServiceUrl = "https://gradle.com/terms-of-service"
+			termsOfServiceAgree = "yes"
+		}
+	}
+}
 
 includeBuild("platforms")
 
