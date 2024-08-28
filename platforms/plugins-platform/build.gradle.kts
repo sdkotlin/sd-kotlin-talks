0b73ecf326de03e83c56f35424b2a795fdbc8b01
Ian Brandt <ian@ianbrandt.com>
2024-08-27 23:15:56 -0700

    Added the JVM Dependency Conflict Detection and Resolution Gradle plugins to the plugins platform.

--- platforms/plugins-platform/build.gradle.kts
+++ platforms/plugins-platform/build.gradle.kts
@@ -7,6 +7,7 @@ group = "org.sdkotlin.platforms"
 dependencies {
 	constraints {
 		api(libs.dependency.analysis.gradle.plugin.dependency)
+		api(libs.jvm.dependency.conflict.resolution.gradle.plugin.dependency)
 		api(libs.kotlin.gradle.plugin.dependency)
 		api(libs.ksp.gradle.plugin.dependency)
 	}
