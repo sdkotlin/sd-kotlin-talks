0fee16c97ca6f7254bf9947dd514ae37e6a0f9e4
Ian Brandt <ian@ianbrandt.com>
2025-02-18 14:53:14 -0800

    Added Java "Unnamed Classes and Instance Main Methods" example.

---
+++ subprojects/kotlin-for-java-devs/src/main/java/org/sdkotlin/intro/java/_02_helloworld/HelloJava21.java
@@ -0,0 +1,12 @@
+/**
+ * Java 21+ is previewing "Unnamed Classes and Instance Main Methods":
+ * <ul>
+ * <li><a href="https://openjdk.org/jeps/445">JEP 445 (Preview 1, JDK 21)</a></li>
+ * <li><a href="https://openjdk.org/jeps/463">JEP 463 (Preview 2, JDK 22)</a></li>
+ * <li><a href="https://openjdk.org/jeps/477">JEP 477 (Preview 3, JDK 23)</a></li>
+ * <li><a href="https://openjdk.org/jeps/495">JEP 495 (Preview 4, JDK 24)</a></li>
+ * </ul>
+ */
+void main() {
+	System.out.println("Hello World");
+}
