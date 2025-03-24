1e2dadb4f0f1b993931a1b2b45fff125ce4417c3
Ian Brandt <ian@ianbrandt.com>
2025-03-23 21:06:22 -0700

    Removed unused runtimeOnly kotlin-reflect dependency and related exclusions.

--- subprojects/kotlin-reflect/build.gradle.kts
+++ subprojects/kotlin-reflect/build.gradle.kts
@@ -7,15 +7,3 @@ dependencies {
 
 	implementation(kotlin("reflect"))
 }
-
-dependencyAnalysis {
-	issues {
-		onRuntimeOnly {
-			exclude(
-				// False positive on `kotlin-reflect` implementation use:
-				// https://github.com/autonomousapps/dependency-analysis-gradle-plugin/issues/1384.
-				"org.jetbrains.kotlin:kotlin-reflect",
-			)
-		}
-	}
-}
