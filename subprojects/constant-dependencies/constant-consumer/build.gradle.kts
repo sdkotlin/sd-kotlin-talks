143a193ec554a65b2c7d7cee89ba2145185af337
Ian Brandt <ian@ianbrandt.com>
2025-06-09 22:58:23 -0700

    Upgraded DAGP to 2.18.0 (appears to fix https://github.com/autonomousapps/dependency-analysis-gradle-plugin/issues/1385).

--- subprojects/constant-dependencies/constant-consumer/build.gradle.kts
+++ subprojects/constant-dependencies/constant-consumer/build.gradle.kts
@@ -6,15 +6,3 @@ dependencies {
 
 	implementation(projects.subprojects.constantDependencies.constantProducer)
 }
-
-dependencyAnalysis {
-	issues {
-		onUnusedDependencies {
-			exclude(
-				// Used but reported as unused:
-				// https://github.com/autonomousapps/dependency-analysis-gradle-plugin/issues/1385.
-				":subprojects:constant-dependencies:constant-producer",
-			)
-		}
-	}
-}
