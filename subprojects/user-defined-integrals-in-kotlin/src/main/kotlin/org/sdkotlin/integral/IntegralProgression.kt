1ebc7d754e5e24680fc6c47dc747ae9a9ac5763b
Ian Brandt <ian@ianbrandt.com>
2025-10-18 16:13:28 -0700

    Moved `IntegralProgressionIterator` to `IntegralProgression`.

--- subprojects/user-defined-integrals-in-kotlin/src/main/kotlin/org/sdkotlin/integral/IntegralProgression.kt
+++ subprojects/user-defined-integrals-in-kotlin/src/main/kotlin/org/sdkotlin/integral/IntegralProgression.kt
@@ -75,4 +75,33 @@ class IntegralProgression<I : Integral<I>> internal constructor(
 			return if (ac >= bc) ac - bc else ac - bc + c
 		}
 	}
+
+	private class IntegralProgressionIterator<I : Integral<I>>(
+		first: I,
+		private val last: I,
+		private val step: I,
+	) : Iterator<I> {
+
+		private var hasNext: Boolean =
+			if (step > step.zero) {
+				first <= last
+			} else {
+				first >= last
+			}
+
+		private var next = if (hasNext) first else last
+
+		override fun hasNext(): Boolean = hasNext
+
+		override fun next(): I {
+			val value = next
+			if (value == last) {
+				if (!hasNext) throw NoSuchElementException()
+				hasNext = false
+			} else {
+				next += step
+			}
+			return value
+		}
+	}
 }
