17225f3895d1ad47d20dd6cae5a4e9fe0f45e20a
Ian Brandt <ian@ianbrandt.com>
2026-09-14 20:40:41 -0700

    Upgraded the Gradle Versions Plugin to 0.63.0.

--- platforms/settings.gradle.kts
+++ platforms/settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 plugins {
 	// Produces the partial report that the root build's `dependencyUpdates`
 	// task merges.
-	id("io.github.ben-manes.versions.settings") version "0.62.0"
+	id("io.github.ben-manes.versions.settings") version "0.63.0"
 }
 
 dependencyResolutionManagement {
