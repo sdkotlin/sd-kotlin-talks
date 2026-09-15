07ebf909ad973a163e4f7a83bf472add76d552c4
Ian Brandt <ian@ianbrandt.com>
2026-09-15 07:17:21 -0700

    Upgraded the Gradle Versions Plugin to 0.63.1.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
 
 plugins {
 	id("com.gradle.develocity") version "4.5.1"
-	id("io.github.ben-manes.versions.settings") version "0.63.0"
+	id("io.github.ben-manes.versions.settings") version "0.63.1"
 	id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
