2ad6cbf0743143019df1002e30f200ca17afb604
Ian Brandt <ian@ianbrandt.com>
2026-09-14 20:59:29 -0700

    Upgraded the Develocity Gradle Plugin to 4.5.1.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 }
 
 plugins {
-	id("com.gradle.develocity") version "4.5.0"
+	id("com.gradle.develocity") version "4.5.1"
 	id("io.github.ben-manes.versions.settings") version "0.63.0"
 	id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
